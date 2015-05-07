93e428648b9baf52926cd7cc5fe2a42cc43c0e74
Luke <lmladek771@gmail.com>
2015-05-07 19:55:38 -0400

    Commented out tests that take too long to run

--- OldTests.sh
+++ OldTests.sh
@@ -1,7 +1,10 @@
 #!/bin/sh 
 
 # This is used to build the Results directory for future comparison
-# Commented out lines are tests that failed to run initially
+
+
+# Commented out lines are either tests that failed to run initially
+# Or tests that took too long to run so were omitted from this script
 
 
 
@@ -32,7 +35,9 @@
 ./RopTool -c luaScipts/x64/call_reg.lua -f x64/expr | tail -n +7 >  Results/Test19 
 ./RopTool -c luaScipts/x64/call_reg.lua -f x64/hostname | tail -n +7 >  Results/Test20
 ./RopTool -c luaScipts/x64/call_reg.lua -f x64/kill | tail -n +7 >  Results/Test21
+#Failed Core Dumped
 #./RopTool -c luaScipts/x64/call_reg.lua -f x64/ksh | tail -n +7 >  Results/Test22
+#Failed Core Dumped
 #./RopTool -c luaScipts/x64/call_reg.lua -f x64/launchctl | tail -n +7 >  Results/Test23
 ./RopTool -c luaScipts/x64/call_reg.lua -f x64/link | tail -n +7 >  Results/Test24
 ./RopTool -c luaScipts/x64/call_reg.lua -f x64/ln  | tail -n +7 >  Results/Test25 
@@ -52,29 +57,39 @@
 ./RopTool -c luaScipts/x64/call_reg.lua -f x64/test | tail -n +7 >  Results/Test39
 ./RopTool -c luaScipts/x64/call_reg.lua -f x64/unlink | tail -n +7 >  Results/Test40
 ./RopTool -c luaScipts/x64/call_reg.lua -f x64/wait4path | tail -n +7 >  Results/Test41 
-#./RopTool -c luaScipts/x64/call_reg.lua -f x64/zsh | tail -n +7 >  Results/Test42
+#Failed Core Dumped
+#./RopTool -c luaScipts/x64/call_reg.lua -f x64/zsh | tail -n +7 >  Results/Test42 
+
 
 
 
 #x86_64_FAT tests ( ommitted MachO-OSX-ppc-and-i386-bash due to size) 
 #As x86 
-./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/call_reg.lua | tail -n +7 >  Results/Test43
-./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/pop_ecx.lua | tail -n +7 >  Results/Test44
-./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/pvtdown.lua | tail -n +7 >  Results/Test45
-./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/pvtfinder.lua | tail -n +7 >  Results/Test46
-./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/pvtfinder2.lua | tail -n +7 >  Results/Test47 
-./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/vcall.lua | tail -n +7 >  Results/Test48
-./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/vcall2.lua | tail -n +7 >  Results/Test49
-./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/transfer_arith_with_branch.lua | tail -n +7 >  Results/Test50
 
+# Too Slow
+#./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/call_reg.lua | tail -n +7 >  Results/Test43
+#./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/pop_ecx.lua | tail -n +7 >  Results/Test44
+./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/pvtdown.lua | tail -n +7 >  Results/Test45
+#./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/pvtfinder.lua | tail -n +7 >  Results/Test46
+# Too Slow
+#./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/pvtfinder2.lua | tail -n +7 >  Results/Test47 
+#./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/vcall.lua | tail -n +7 >  Results/Test48
+# Too Slow
+#./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/vcall2.lua | tail -n +7 >  Results/Test49
+# Too Slow
+#./RopTool -a x86 -f x86_64_FAT/bash -c luaScipts/x86/transfer_arith_with_branch.lua | tail -n +7 >  Results/Test50
+
+
+
+# # Way Too Slow
 ./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/call_reg.lua | tail -n +7 >  Results/Test51
-./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/pop_ecx.lua | tail -n +7 >  Results/Test52
-./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/pvtdown.lua | tail -n +7 >  Results/Test53 
-./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/pvtfinder.lua | tail -n +7 >  Results/Test54
-./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/pvtfinder2.lua | tail -n +7 >  Results/Test55
-./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/vcall.lua | tail -n +7 >  Results/Test56 
-./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/vcall2.lua | tail -n +7 >  Results/Test57 
-./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/transfer_arith_with_branch.lua | tail -n +7 >  Results/Test58 
+#./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/pop_ecx.lua | tail -n +7 >  Results/Test52
+#./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/pvtdown.lua | tail -n +7 >  Results/Test53 
+#./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/pvtfinder.lua | tail -n +7 >  Results/Test54
+#./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/pvtfinder2.lua | tail -n +7 >  Results/Test55
+#./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/vcall.lua | tail -n +7 >  Results/Test56 
+#./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/vcall2.lua | tail -n +7 >  Results/Test57 
+#./RopTool -a x86 -f x86_64_FAT/sh -c luaScipts/x86/transfer_arith_with_branch.lua | tail -n +7 >  Results/Test58 
 
 
 ./RopTool -a x86 -f x86_64_FAT/sync -c luaScipts/x86/call_reg.lua | tail -n +7 >  Results/Test59
